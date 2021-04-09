f50c6a54f492431ac6e3c1d4363e238383ad61e7
rahul <rahulniraula47@gmail.com>
2021-04-08 19:56:52 -0700

    Initial Commit

---
+++ Canada Covid-19 Vaccine Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Canada Covid-19 Vaccine Tracker
+//
+//  Created by Rahul Niraula on 2021-04-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Canada Covid-19 Vaccine TrackerTests/Canada_Covid_19_Vaccine_TrackerTests.swift
@@ -0,0 +1,33 @@
+//
+//  Canada_Covid_19_Vaccine_TrackerTests.swift
+//  Canada Covid-19 Vaccine TrackerTests
+//
+//  Created by Rahul Niraula on 2021-04-08.
+//
+
+import XCTest
+@testable import Canada_Covid_19_Vaccine_Tracker
+
+class Canada_Covid_19_Vaccine_TrackerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
